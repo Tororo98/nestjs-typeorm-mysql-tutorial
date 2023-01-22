f3c4e1a8272fbe1a38880ab361e46e779e11dde0
Tororo98 <sebastiantofra98@javerianacali.edu.co>
2023-01-22 14:59:14 -0500

    added following users to profile

--- src/typeorm/entities/Profile.ts
+++ src/typeorm/entities/Profile.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./User";
 
 @Entity({name: 'user_profiles'})
 export class Profile {
@@ -16,4 +17,7 @@ export class Profile {
 
     @Column()
     dob: string;
+
+    @OneToMany(() => User, (user) => user.username)
+    following: User[];
 }
